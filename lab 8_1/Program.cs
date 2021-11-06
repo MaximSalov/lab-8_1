3f1994e488b2abdf3c5f51773c2befd18be8c72a
ПОльзователь <@USER436809363>
2021-11-06 14:19:25 +0300

    Добавьте файлы проекта.

---
+++ lab 8_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab_8_1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("C:/Флешка");
+            Console.WriteLine("Список каталогов");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("Список подкаталогов");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Список файлов");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
